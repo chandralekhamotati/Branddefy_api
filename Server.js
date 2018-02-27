801059044f1226afdab47b761931d302a3f28156
chandralekhamotati <chandralekhamotati@users.noreply.github.com>
2018-02-26 20:52:21 -0500

    Update Server.js

--- Server.js
+++ Server.js
@@ -16,7 +16,7 @@ REST.prototype.connectMysql = function() {
         connectionLimit : 100,
         host     : 'localhost',
         user     : 'root',
-        password : '',
+        password : 'brandefy',
         database : 'brandefy_app',
         debug    :  false
     });
